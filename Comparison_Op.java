c93294e6f6a4e8d34549d48f9a94cf58b35f8603
POORNIKA R <poornika0406@gmail.com>
2026-05-22 10:53:10 +0530

    Day 2 (Comp_operators)

---
+++ Comparison_Op.java
@@ -0,0 +1,28 @@
+public class Comparison_Op {
+   
+    public static void main(String[] args) {
+        //Arithmitic operation
+        int a =10;
+        int b= 20;
+        boolean c ,d,e,f,g,h;
+         c = (a==b);
+         d= (a!=b);
+         e=(a<b);
+         f=(a>b);
+         g=(a>=b);
+         h=(a<=b);
+
+        System.out.println("Integer a = 10 \nInteger b = 20 ");
+        System.out.println( "Both Integers are equal: "+ c );
+        System.out.println( "Both integers are not equal: "+ d );
+        System.out.println( "Integer a is less than integer b: "+ e );
+       
+        System.out.println( "Integer a is greater than integer b: "+ f);
+        System.out.println( "Integera is greater than or equal to b: "+ g);
+        System.out.println( "Integer is lesser than or equal to b: "+ h);
+
+     
+    }
+}
+    
+
