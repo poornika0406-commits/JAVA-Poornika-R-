94092a9f6a4547ac4035eac8d76f54f7019a25b4
POORNIKA R <poornika0406@gmail.com>
2026-05-22 10:38:05 +0530

    Day 2 (Operators)

---
+++ Operator.java
@@ -0,0 +1,18 @@
+public class Operator {
+    public static void main(String[] args) {
+        //Arithmitic operation
+        int a =10;
+        int b= 20;
+        int sum = a+b ;
+        int diff = a-b;
+        int multiply= a*b;
+        int div =b/a;
+
+       
+        System.out.println( "Sum of two integers: "+ sum );
+        System.out.println( "Difference of two integers: "+ diff );
+        System.out.println( "Product of two integers: "+ multiply );
+        System.out.println( "Division of two integers: "+ div);
+     
+    }
+}
